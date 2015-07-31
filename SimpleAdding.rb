4748497a8a55a7e45f17a6d92a6de21528b45aa7
Tonikim <dhrhyyc@naver.com>
2015-07-31 16:37:36 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,11 @@
+def SimpleAdding(num)
+
+  # code goes here
+  
+  return ((num+1)*num)/2
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)  
