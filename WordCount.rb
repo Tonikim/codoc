3be92b9022cea085644f2617960e5a2ebb289880
Tonikim <dhrhyyc@naver.com>
2015-07-31 18:02:29 +0900

    Create WordCount.rb

---
+++ WordCount.rb
@@ -0,0 +1,30 @@
+def WordCount(str)
+
+  # code goes here
+  
+  return str.scan(/[[:alpha:]]+/).count
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+WordCount(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
